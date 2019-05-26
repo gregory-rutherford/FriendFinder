f017a98e9ab6d64899e25829120f8a12914f30a2
Gregory Rutherford <46570985+gregory-rutherford@users.noreply.github.com>
2019-05-25 20:24:47 -0500

    init commit

---
+++ server.js
@@ -0,0 +1,10 @@
+//Dependencies 
+var express = require("express");
+var path = require("path");
+//================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+//Set up app for data parsing
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
