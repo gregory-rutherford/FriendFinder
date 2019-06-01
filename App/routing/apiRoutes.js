6188bef95cef37568f717636ca6d7c0d14b89558
Gregory Rutherford <46570985+gregory-rutherford@users.noreply.github.com>
2019-05-31 21:02:40 -0500

    got rid of min jquery so ajax would work, made post route for friends api and can push user scores and info into api

--- App/routing/apiRoutes.js
+++ App/routing/apiRoutes.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var friends = require("../Data/friends");
+var match = [];
 
 router.use(function(req, res, next) {
   console.log("Time:", Date.now());
@@ -13,7 +14,6 @@ router.get("/api/friends", function(req, res) {
 
 router.get("/api/friends/:friend", function(req, res) {
   var chosen = req.params.friend;
-
   console.log(chosen);
   //return friend if routeName matches input
   for (var i = 0; i < friends.length; i++) {
@@ -23,6 +23,28 @@ router.get("/api/friends/:friend", function(req, res) {
   }
 
   return res.json(false);
+});
+
+router.post("/api/friends", function(req, res) {
+  
+  friends.push(req.body);
+  res.json(req.body);
+  var difference = 0;
+  match = [];
+  //grab the users score and converts it to numbers for comparison
+  var userScore = req.body.scores;
+  var storage = userScore.toString();
+  var userScoreNum = storage.split(",").map(function(item){
+    return parseInt(item, 10);
+  })
+  console.log(userScoreNum);
+  // loop through friends array
+  for (var i = 0; i < friends.length; i++){
+    var friendScores = friends[i].scores;
+    console.log(friendScores);
+  }
+  //do the math on all the scores
+  //return the least amount of difference
 });
 
 module.exports = router;
